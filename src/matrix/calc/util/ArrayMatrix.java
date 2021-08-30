4c0b94fce11f5d1632cacd89e9cde11450a8f665
mmadmin <mex128@outlook.com>
2021-08-30 18:56:18 +0200

    Changeing Testclass
    Testclass has been changed in order to provide more tests and information.
    Changes in the implementation in the rule of Sarrus.

--- src/matrix/calc/util/ArrayMatrix.java
+++ src/matrix/calc/util/ArrayMatrix.java
@@ -1,5 +1,6 @@
 package matrix.calc.util;
 
+
 public class ArrayMatrix implements Matrix{
     private double[][] arr;
 
@@ -7,7 +8,7 @@ public class ArrayMatrix implements Matrix{
         this.arr = array;
     }
     @Override
-    public boolean multiplyable(Matrix mat2) {
+    public boolean multiplyable(Matrix mat2) { // checking if two matrix can be multiplied
         assert mat2 instanceof ArrayMatrix;
         return this.countColl() == ((ArrayMatrix) mat2).countRows();
     }
@@ -24,7 +25,7 @@ public class ArrayMatrix implements Matrix{
         return new ArrayMatrix(result);
     }
 
-    public double sclproduct(double[] array, double scalar){
+    public double sclproduct(double[] array, double scalar){ // implements a scalarproduct of a vector and a scalar
         double result = 0;
         for(int i = 0; i < array.length; i++){
             result += (scalar * array[i]);
@@ -82,15 +83,24 @@ public class ArrayMatrix implements Matrix{
 
     @Override
     public double det() {
+       double result = 0; // Line 86 - 98 implements the rule of Sarrus
         if(this.countColl() == 2 && this.countRows() == 2){
-            return (this.arr[0][0]*this.arr[1][1])-(this.arr[0][1]*this.arr[1][0]);
+            result = (this.arr[0][0]*this.arr[1][1])-(this.arr[0][1]*this.arr[1][0]);
+            return result;
         }else{
             if(this.countRows() == 3 && this.countColl() == 3){
-                return 0; // not implemented yet
+                result = (this.arr[0][0] * this.arr[1][1] * this.arr[2][2])
+                        + (this.arr[0][1] * this.arr[1][2] * this.arr[2][0])
+                        + (this.arr[0][2] * this.arr[1][0] * this.arr[2][1])
+                        - (this.arr[2][0] * this.arr[1][1] * this.arr[0][2])
+                        - (this.arr[2][1] * this.arr[1][2] * this.arr[0][0])
+                        - (this.arr[2][2] * this.arr[1][0] * this.arr[0][2]);
+                return result;
             }
         }
 
-        return 0;
+        // needs a logic that can calculate dets of a matrix greater than 3x3
+        return result;
     }
 
     public int countColl(){
@@ -101,7 +111,7 @@ public class ArrayMatrix implements Matrix{
         return this.arr.length;
     }
 
-    public double[] getColl(int i){
+    public double[] getColl(int i){ // Writes a collum specified by index in an Array.
         double[] result = new double[this.countRows()];
         for(int k = 0; k < result.length; k++){
             result[k] = this.arr[k][i];
@@ -111,5 +121,11 @@ public class ArrayMatrix implements Matrix{
 
     public double[] getRow(int i){
         return this.arr[i];
+    }
+
+    public static void main(String[] args) {
+        double[][] array1 = new double[][]{{1,2,3}, {4,5,6}, {7,8,9}};
+        ArrayMatrix test = new ArrayMatrix(array1);
+        System.out.println(test.det());
     }
 }
--- src/matrix/calc/util/MatrixTester.java
+++ src/matrix/calc/util/MatrixTester.java
@@ -4,16 +4,46 @@ import org.junit.Test;
 
 public class MatrixTester {
     double[][] array = new double[][]{{1, 2, 3}, {3,2,1}};
+    double[][] array1 = new double[][]{{1,2,3}, {4,5,6}, {7,8,9}};
+    double[][] array2 = new double[][]{{5, -1, 9}, {-1, 6, -1}, {9, -1, 7}};
     ArrayMatrix test = new ArrayMatrix(array);
+    ArrayMatrix test1 = new ArrayMatrix(array1);
+    ArrayMatrix test2 = new ArrayMatrix(array2);
+
     @Test
     public void countRow1(){
         assert test.countRows() == 2;
+        System.out.println("expected 2; actual "+test.countRows());
     }
+
+    @Test
+    public void countRow2(){
+        assert test1.countRows() == 3;
+        System.out.println("expected 3" + " actual " + test1.countRows());
+    }
+
     @Test
     public void countColl1(){
         assert test.countColl() == 3;
+        System.out.println("expected 3; actual " + test.countRows());
     }
 
-    
+    @Test
+    public void countColl2(){
+        assert test1.countColl() == 3;
+        System.out.println("expected 3; actual "+test1.countColl());
+    }
+
+    @Test
+    public void det1(){
+        assert test1.det() == 0;
+        System.out.println("expected 0; actual " + test1.det());
+    }
+
+    @Test
+    public void det2(){
+        assert test2.det() == -270;
+        System.out.println("expected -270; actual " + test2.det());
+    }
 
 }
